f40ff19772876d7eba3545e964cdeccfbfd6963b
Johan Dettmar <dettmar@Macintosh-6.SpeedportNEO09012801000249>
2017-07-07 15:04:04 +0200

    Turing machine outputter added

---
+++ Turing.py
@@ -0,0 +1,36 @@
+from ExecuteTM import ExecuteTM
+from VisualTM import VisualTM
+import os
+import sys
+
+class Turing:
+	"""
+	This class executes the Turing machine and build a visualized pdf
+	from the command line, like so:
+	`python3 Turing.py <tm txt file name> <comma separated inputstring>`
+	for example:
+	`python3 Turing.py col 0,1,1,0`
+	"""
+	def __init__(self, turing_machine, inputstrings = "", filename = "tm.pdf"):
+
+		turing_machine += ".txt" if turing_machine[-4:] != ".txt" else ""
+
+		if not os.path.isfile("tapes/{}".format(turing_machine)):
+			raise Exception("No such Turing machine:", "tapes/{}".format(turing_machine))
+
+		self.output_file(turing_machine, inputstrings, filename)
+
+	def output_file(self, turing_machine, inputstrings, filename):
+
+		print("Executing", "tapes/{}".format(turing_machine))
+		print("With inputstring(s)", inputstrings)
+		tm = ExecuteTM()
+		tm._parse_file("tapes/{}".format(turing_machine))
+		tm.exec_TM(inputstrings)
+		vis = VisualTM()
+
+		vis.write_file(tm.get_steps())
+		#vis.visualize()
+
+
+Turing(*sys.argv[1:])
