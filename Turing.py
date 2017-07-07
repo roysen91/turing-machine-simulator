8f7986df90d0b8a48b27113d6488064c04330824
Johan Dettmar <dettmar@Macintosh-6.SpeedportNEO09012801000249>
2017-07-07 16:59:12 +0200

    Added opening pdf by viewer options

--- Turing.py
+++ Turing.py
@@ -11,26 +11,29 @@ class Turing:
 	for example:
 	`python3 Turing.py col 0,1,1,0`
 	"""
-	def __init__(self, turing_machine, inputstrings = "", filename = "tm"):
+	def __init__(self, turing_machine, inputstrings = "", filename = "tm", viewername = None):
 
 		turing_machine += ".txt" if turing_machine[-4:] != ".txt" else ""
 
 		if not os.path.isfile("tapes/{}".format(turing_machine)):
 			raise Exception("No such Turing machine:", "tapes/{}".format(turing_machine))
 
-		self.output_file(turing_machine, inputstrings, filename)
+		self.output_file(turing_machine, inputstrings, filename, viewername)
 
-	def output_file(self, turing_machine, inputstrings, filename):
+	def output_file(self, turing_machine, inputstrings, filename, viewername = None):
 
 		print("Executing", "tapes/{}".format(turing_machine))
 		print("With inputstring(s)", inputstrings)
+		print("Opening with", viewername)
 		tm = ExecuteTM()
 		tm._parse_file("tapes/{}".format(turing_machine))
 		tm.exec_TM(inputstrings)
 		vis = VisualTM()
-
 		vis.write_file(tm.get_steps(), filename)
-		#vis.visualize()
+
+		if viewername:
+			vis.set_viewer(viewername)
+			vis.visualize()
 
 
 Turing(*sys.argv[1:])
